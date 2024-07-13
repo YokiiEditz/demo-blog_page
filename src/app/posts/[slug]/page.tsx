67500708d25d32bdf66cfb54c111edd2298b1a2c
Yoki <thekingmaker0012@gmail.com>
2024-07-13 16:07:34 +0530

    posts added

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,13 @@
+const PostPage = () => {
+  return (
+    <div>
+      <iframe
+        width="420"
+        height="315"
+        src="https://www.youtube.com/embed/tgbNymZ7vqY"
+      ></iframe>
+    </div>
+  );
+};
+
+export default PostPage;
